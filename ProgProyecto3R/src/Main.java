e30b084e47bf95108860e8df3a0272e3ba1684b9
kevin31000 <kevin.arnaiz@opendeusto,es>
2019-09-19 12:32:37 +0200

    Prueba commit kevinn

--- ProgProyecto3R/src/Main.java
+++ ProgProyecto3R/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Popeye");
-		System.out.println("Prueba de commit");
+		System.out.println("Prueba de commit, segundo intento");
 	}
 
 }
