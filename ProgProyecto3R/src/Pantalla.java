fb5dc5e59e2097013b6c7f815f07e5fb16d8df39
IzaiAnda <izai.anda@opendeusto.es>
2019-10-04 12:37:32 +0200

    SQLite jdbc Intents de creacion de pantalla

--- ProgProyecto3R/src/Pantalla.java
+++ ProgProyecto3R/src/Pantalla.java
@@ -1,7 +1,9 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.Dimension;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -13,13 +15,14 @@ public class Pantalla extends JFrame{
 		
 		JPanel arriba = new JPanel();
 		arriba.setBackground(Color.RED);
-		arriba.setSize(750, 200);
 		
-		JPanel abajo = new JPanel();
-		abajo.setBackground(Color.BLUE);
 		
-		cp.add(arriba, BorderLayout.NORTH);
-		cp.add(abajo, BorderLayout.SOUTH);
+		
+		JButton jugar = new JButton("Jugar");
+		jugar.setPreferredSize(new Dimension(200,100));
+		
+		arriba.add(jugar );
+		cp.add(arriba);
 		this.setSize(750, 500);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setTitle("ChalMonsters");
