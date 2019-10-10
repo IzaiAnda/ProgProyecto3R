c8c9209bf84253c0bfdd09510f3e06f6dd4161c0
kevin31000 <kevin.arnaiz@opendeusto,es>
2019-10-10 18:46:58 +0200

    Textfield inicio sesion + eventos

--- ProgProyecto3R/src/ventanas/VentanaInicioJuego.java
+++ ProgProyecto3R/src/ventanas/VentanaInicioJuego.java
@@ -3,12 +3,17 @@ package ventanas;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
 public class VentanaInicioJuego extends JFrame {
@@ -20,9 +25,11 @@ public class VentanaInicioJuego extends JFrame {
 
 	private JPanel contentpane = new JPanel();
 	
-	JLabel labelfondo = new JLabel();
 	JButton btnJugar = new JButton();
 	JLabel labelPkm = new JLabel();
+	JLabel labelfondo = new JLabel();
+	JLabel labelLogin = new JLabel();
+	JTextField textLogin = new JTextField();
 	
 
 	public VentanaInicioJuego(int altura, int anchura) {
@@ -33,7 +40,7 @@ public class VentanaInicioJuego extends JFrame {
 		contentpane.setLayout(null);
 		
 		labelPkm.setIcon(new ImageIcon(VentanaInicioJuego.class.getResource("/images/pkm.png")));
-		labelPkm.setBounds(25, -50, altura, anchura);
+		labelPkm.setBounds(25, -80, altura, anchura);
 		contentpane.add(labelPkm);
 		
 		btnJugar.setText("Pulse para jugar");
@@ -41,13 +48,63 @@ public class VentanaInicioJuego extends JFrame {
 		btnJugar.setBounds(250, 300, 200, 20);
 		contentpane.add(btnJugar);
 		
-		labelfondo.setIcon(new ImageIcon(VentanaInicioJuego.class.getResource("/images/background.png")));
-		labelfondo.setBounds(0, 0, altura, anchura);
-		contentpane.add(labelfondo);
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(altura, anchura);
 		setTitle("DeustMon");
+		
+		
+		//Eventos
+		btnJugar.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+				btnJugar.setVisible(false);
+				contentpane.remove(btnJugar);
+				
+				textLogin.setBounds(250, 300, 200, 20);
+				textLogin.setText("Introduce usuario:");
+				contentpane.add(textLogin);
+				
+				//No consigo que se vea
+				
+				/*labelLogin.setText("Introduzca usuario:");
+				labelLogin.setBounds(0, 0, 200, 20);
+				labelLogin.setBackground(new Color(255,175,175));
+				contentpane.add(labelLogin);*/
+				
+			}
+		});
+		textLogin.addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) {
+				switch (e.getKeyCode()) {
+					case KeyEvent.VK_ENTER:
+						//Abrir ventana seleccion
+						
+						break;
+				}
+				
+			}
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+	
+		labelfondo.setIcon(new ImageIcon(VentanaInicioJuego.class.getResource("/images/background.png")));
+		labelfondo.setBounds(0, 0, altura, anchura);
+		contentpane.add(labelfondo);
 	}
 
 }
