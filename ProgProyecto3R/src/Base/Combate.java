8547d418ebac43139b24311055d97221b42bb83e
IzaiAnda <izai.anda@opendeusto.es>
2019-11-05 16:18:35 +0100

    Calculo del daño de combate creado y su JUnit

---
+++ ProgProyecto3R/src/Base/Combate.java
@@ -0,0 +1,32 @@
+package Base;
+
+public class Combate extends Thread  {
+	
+	Monster mon1 = new Monster("Uno",200,20,20,20);
+	Monster mon2 = new Monster("Dos",200,20,20,20);
+	
+	//efectividad(1 normal, 2 ventaja), ataque del pokemon, poder del ataque, defensa del enemigo
+	
+	public static int calculo(boolean e, int a, int p, int d) {
+		
+		int salida = 0;
+		
+		double dmg = 0.44 * a * p / d;
+		
+		if (e) {
+			dmg *= 2;
+		}
+		
+		salida =(int) dmg; 
+		
+		return salida;	
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		super.run();
+	}
+	
+	
+}
---
+++ ProgProyecto3R/src/Test/CombateTest.java
@@ -0,0 +1,27 @@
+package Test;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import Base.Combate;
+
+class CombateTest {
+
+	@Test
+	void testCalculo() {
+		
+		int	a = 20;
+		int p = 40;
+		int	d = 20;
+		
+		double	dmg = 0.196;
+		
+		int r = (int) dmg;
+		assertEquals(r, Combate.calculo(false,a,p,d));
+		
+		
+	}
+
+}
