dfd437b60affc8c2da56a9adbacb368b6be804fb
IzaiAnda <izai.anda@opendeusto.es>
2019-11-15 10:35:17 +0100

    Daño de combate Terminado

--- ProgProyecto3R/src/baseJuego/Combate.java
+++ ProgProyecto3R/src/baseJuego/Combate.java
@@ -8,11 +8,7 @@ import moves.Move;
 
 public class Combate{
 
-	static MonsterPlant mon = new MonsterPlant("Plantita",100,100,100,100);
-	static MonsterFire mon2 = new MonsterFire("Fuegillo",100,100,100,100);
-	static MonsterWater mon3 = new MonsterWater("Gotita",50,50,50,50);
 
-	static Move move = new Move("Llamarada", 10);
 	
 	// efectividad(1 normal, 2 ventaja), ataque del pokemon, poder del ataque,
 	// defensa del enemigo
@@ -32,16 +28,25 @@ public class Combate{
 		return exit;
 	}
 	
-	public static int damageTaken(Monster attacker, Monster defender, Move action) {
+	public static Monster damageTaken(Monster attacker, Monster defender, Move action) {
 		
 		boolean advantage = defender.efectividad(attacker.getTipe());
-		System.out.println(advantage);
 		
-		return calculo(advantage, attacker.getattack(), action.getDamage(), defender.getdefense());
+		int damageDone = calculo(advantage, attacker.getattack(), action.getDamage(), defender.getdefense());
+		int defenderLifePoints = defender.getlifePoints() -damageDone ;
+		
+		defender.setlifePoints(defenderLifePoints);
+		return defender;
 		
 	}
 	
 	public static void main(String[] args) {
+		
+		 MonsterPlant mon = new MonsterPlant("Plantita",100,100,100,100);
+		 MonsterFire mon2 = new MonsterFire("Fuegillo",100,100,100,100);
+		 MonsterWater mon3 = new MonsterWater("Gotita",50,50,50,50);
+
+		 Move move = new Move("Llamarada", 10);
 		
 		System.out.println(damageTaken(mon, mon2, move));
 		
