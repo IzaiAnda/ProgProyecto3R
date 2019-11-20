f65987e0543c5f5813c59685adb1749367227e42
IzaiAnda <44356898+IzaiAnda@users.noreply.github.com>
2019-11-20 15:01:34 +0100

    Haciendo Options

--- ProgProyecto3R/src/ventanas/OptionsWindow.java
+++ ProgProyecto3R/src/ventanas/OptionsWindow.java
@@ -10,7 +10,9 @@ public class OptionsWindow extends JFrame {
 	private static final long serialVersionUID = 1L;
 
 	public OptionsWindow(int altura, int anchura) {
-
+		
+		
+		
 		setSize(altura, anchura);
 		setTitle("Options");
 	}
--- ProgProyecto3R/src/ventanas/PokedexWindow.java
+++ ProgProyecto3R/src/ventanas/PokedexWindow.java
@@ -1,6 +1,12 @@
 package ventanas;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class PokedexWindow extends JFrame {
 
@@ -9,10 +15,29 @@ public class PokedexWindow extends JFrame {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	
+	private JMenuBar barr;
+	private JMenu menu1;
+	private JMenuItem item1;
+	
 	public PokedexWindow(int altura, int anchura) {
-
+		
+		barr = new JMenuBar();
+		setJMenuBar(barr);
+		
+		menu1 = new JMenu("Menu");
+		barr.add(menu1);
+		
+		item1 = new JMenuItem("Item");
+		menu1.add(item1);
+		
+		menu1.addSeparator();
+		
 		setSize(altura, anchura);
 		setTitle("MonsterWiki");
 	}
-
+	public static void main(String[] args) {
+		PokedexWindow pokedexWindow = new PokedexWindow(750, 422);
+		pokedexWindow.setVisible(true);
+	}
 }
