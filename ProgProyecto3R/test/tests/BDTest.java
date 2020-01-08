e80445ebecbda1d4e13e8e2c5ce7246450b840cc
IzaiAnda <44356898+IzaiAnda@users.noreply.github.com>
2020-01-08 20:23:28 +0100

    LevelGame y cambios que no recuerdo que hacian

---
+++ ProgProyecto3R/test/tests/BDTest.java
@@ -0,0 +1,24 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+
+import baseJuego.BD;
+
+import org.junit.Test;
+
+public class BDTest {
+	
+	@Before
+	private void setUp() {
+		BD.startBD();
+	}
+	
+	@Test
+	public void test() {
+		BD.create();
+
+	}
+
+}
