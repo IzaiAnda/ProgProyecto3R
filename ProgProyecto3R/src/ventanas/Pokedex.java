9b8bca980759fbda4a98904e50d3b94407b0866d
Q <Q@DESKTOP-KT5VP8Q.home>
2020-01-08 01:48:43 +0100

    pokedex

--- ProgProyecto3R/src/ventanas/Pokedex.java
+++ ProgProyecto3R/src/ventanas/Pokedex.java
@@ -1,7 +1,10 @@
 package ventanas;
 
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
@@ -18,43 +21,43 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
 
 import baseJuego.BD;
 import monsters.Monster;
 
+public class Pokedex extends JFrame {
 
+	static ImageIcon icon;
 
-public class Pokedex extends JFrame{
-
-		/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
-		static int count = 0;
-		
+	static int count = 0;
+
 	public Pokedex() {
-		
-		this.setSize(800,400);
-		
+
+		this.setSize(800, 420);
+
 		JPanel container = new JPanel();
-		JTextArea monsters = new JTextArea();
+		JTextArea monsters = new JTextArea("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n  ");
 		JScrollPane scrPane = new JScrollPane(monsters);
 		JTextField searcher = new JTextField();
 		JLabel image = new JLabel();
 		JButton search = new JButton("Search");
 		JButton back = new JButton("Back");
-		ArrayList<Image> images = new ArrayList<Image>();
-		
-		
+		Font font = new Font("Serif Bold", Font.BOLD, 14);
+		JLabel labelBackGround = new JLabel();
+		JLabel backImage = new JLabel();
 		
 		LinkedList<Monster> monsterList = new LinkedList<Monster>();
-		
-		
+
 		monsterList = BD.selectAllMonsters();
 		PokedexWindow.addMonsters(monsterList);
 		
-		ArrayList<JButton> buttons = new ArrayList<JButton>();
+		
 
+		ArrayList<JButton> buttons = new ArrayList<JButton>();
+		
+		
 		JLabel title = new JLabel("Monsters");
 		JButton mons1 = new JButton();
 		buttons.add(mons1);
@@ -86,43 +89,53 @@ public class Pokedex extends JFrame{
 		buttons.add(mons14);
 		JButton mons15 = new JButton();
 		buttons.add(mons15);
-		
-		
+
 		for (int i = 0; i < monsterList.size(); i++) {
 			buttons.get(i).setText(monsterList.get(i).getName());
-			
+			buttons.get(i).setFont(font);
 		}
-		
+
 		JTextArea monstersData = new JTextArea();
+
+		//container.setSize(800, 400);
 		
 		
-		container.setSize(100, 100);
+		
 		setLayout(null);
-		scrPane.setBounds(10, 50, 150, 300);
+		scrPane.setBounds(10, 50, 150, 320);
 		this.add(scrPane);
-		
+
 		image.setBounds(525, 15, 180, 180);
-		
 		this.add(image);
 		
+		backImage.setBounds(485, 15, 270, 330);
+		this.add(backImage);
+
 		title.setBounds(10, 15, 75, 25);
 		this.add(title);
-		
-		monstersData.setBounds(450, 200, 300, 150);
+
+		monstersData.setBounds(500, 220, 240, 110);
 		this.add(monstersData);
-		
+
 		searcher.setBounds(100, 15, 100, 25);
 		this.add(searcher);
-		
+
 		search.setBounds(210, 15, 75, 25);
 		this.add(search);
-		
-        back.setBounds(300, 15, 75, 25);
+
+		back.setBounds(300, 15, 75, 25);
 		this.add(back);
 		
+		labelBackGround.setBounds(0, 0, 800, 400);
+		this.add(labelBackGround);
 		
+		labelBackGround.setIcon(new ImageIcon(Pokedex.class.getResource("/images/backPok.png")));
+		
+		backImage.setIcon(new ImageIcon(Pokedex.class.getResource("/images/backImg.jpg")));
+		
+
 		for (int i = 0; i < monsterList.size(); i++) {
-				
+
 			count = i;
 			String name = monsterList.get(count).getName();
 			String type = monsterList.get(count).getTypeString();
@@ -131,77 +144,108 @@ public class Pokedex extends JFrame{
 			int defense = monsterList.get(count).getDefense();
 			int speed = monsterList.get(count).getSpeed();
 			
+			
+
 			buttons.get(i).addActionListener(new ActionListener() {
-				
-				
+
 				@Override
 				public void actionPerformed(ActionEvent e) {
-					
+
 					monstersData.setText(null);
-					
-					monstersData.setText(	"Name: " + 	name + 	"\n" + 
-											"Type: " + 	type +	 "\n" + 
-											"HP: " 	+	 hp + 	"\n" + 
-											"Attack: " + attack + "\n" + 
-											"Defense: " + defense + "\n" + 
-											"Speed: " + speed + 	"\n");
-					
-					
-					
+
+					monstersData.setText("  Name: " + name + "\n" + "  Type: " + type + "\n" + "  HP: " + hp + "\n"
+							+ "  Attack: " + attack + "\n" + "  Defense: " + defense + "\n" + "  Speed: " + speed + "\n");
+
+					icon = new ImageIcon(Pokedex.class.getResource("/images/monsters/" + name + ".png"));
+					image.setIcon(icon);
+
 				}
-				
 			});
-			
+
 		}
 		
-	
+		
+
+		back.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+				dispose();
+
+			}
+
+		});
+
+		search.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+				String name = searcher.getText();
+
+				for (int i = 0; i < buttons.size(); i++) {
+
+					String nameb = buttons.get(i).getText();
+
+					if (name.equals(nameb)) {
+						buttons.get(i).doClick();
+					}
+				}
+
+			}
+
+		});
+
 		monsters.add(mons1);
-		mons1.setBounds(5, 5, 100, 25);
-		
+		mons1.setBounds(5, 5, 120, 25);
+
 		monsters.add(mons2);
-		mons2.setBounds(5, 35, 100, 25);
-		
+		mons2.setBounds(5, 35, 120, 25);
+
 		monsters.add(mons3);
-		mons3.setBounds(5, 65, 100, 25);
-		
+		mons3.setBounds(5, 65, 120, 25);
+
 		monsters.add(mons4);
-		mons4.setBounds(5, 95, 100, 25);
-		
+		mons4.setBounds(5, 95, 120, 25);
+
 		monsters.add(mons5);
-		mons5.setBounds(5, 125, 100, 25);
-		
+		mons5.setBounds(5, 125, 120, 25);
+
 		monsters.add(mons6);
-		mons6.setBounds(5, 155, 100, 25);
-		
+		mons6.setBounds(5, 155, 120, 25);
+
 		monsters.add(mons7);
-		mons7.setBounds(5, 185, 100, 25);
-		
+		mons7.setBounds(5, 185, 120, 25);
+
 		monsters.add(mons8);
-		mons8.setBounds(5, 215, 100, 25);
-		
+		mons8.setBounds(5, 215, 120, 25);
+
 		monsters.add(mons9);
-		mons9.setBounds(5, 245, 100, 25);
-		
+		mons9.setBounds(5, 245, 120, 25);
+
 		monsters.add(mons10);
-		mons10.setBounds(5, 275, 100, 25);
-		
+		mons10.setBounds(5, 275, 120, 25);
+
 		monsters.add(mons11);
-		mons11.setBounds(5, 305, 100, 25);
-		
+		mons11.setBounds(5, 305, 120, 25);
+
 		monsters.add(mons12);
-		mons12.setBounds(5, 335, 100, 25);
-		
+		mons12.setBounds(5, 335, 120, 25);
+
 		monsters.add(mons13);
-		mons13.setBounds(5, 365, 100, 25);
-		
+		mons13.setBounds(5, 365, 120, 25);
+
 		monsters.add(mons14);
-		mons14.setBounds(5, 395, 100, 25);
-		
+		mons14.setBounds(5, 395, 120, 25);
+
 		monsters.add(mons15);
-		mons15.setBounds(5, 425, 100, 25);
+		mons15.setBounds(5, 425, 120, 25);
+		
 		
 		
 	}
+	
 	
 	public static void main(String[] args) {
 		Pokedex pokedex = new Pokedex();
